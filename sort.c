df8c27fcb3181a28f918acf1db1f618a1380c908
DESKTOP-2CHP3MC\Kaustubh Chaudhuri <kaustubhc2014@gmail.com>
2018-04-09 19:03:32 -0700

    Added new python code

---
+++ sort.c
@@ -0,0 +1,15 @@
+// sorting
+#include <stdio.h>
+
+void main()
+{
+    int i, j, a, n = 10;
+    int number[10] = {9,3,1,8,2,6,5,7,2,0};
+//------------------------
+// write your solution out here
+
+//------------------------
+    printf("output -> \n");
+    for (i = 0; i < n; ++i)
+        printf("%d\n", number[i]); // output should print sorted numbers
+}
